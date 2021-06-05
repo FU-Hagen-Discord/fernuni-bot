3bd8f49550531817bdd30656ce1e7c6b3217ede7
samari-k <safi.kuehn@gmx.de>
2021-06-05 22:59:41 +0200

    change "group" to "category" and lower all category names

--- cogs/links.py
+++ cogs/links.py
@@ -23,56 +23,57 @@ class Links(commands.Cog):
 
     @help(
         category="links",
-        brief="Zeigt die Links an, die in diesem Channel (evtl. unter Berücksichtigung einer Gruppe) hinterlegt sind.",
+        brief="Zeigt die Links an, die in diesem Channel (evtl. unter Berücksichtigung einer Kategorie) hinterlegt sind.",
         parameters={
-            "group": "*(optional)* Schränkt die angezeigten Links auf die übergebene Gruppe ein. "
+            "category": "*(optional)* Schränkt die angezeigten Links auf die übergebene Kategorie ein. "
         }
     )
     @commands.command(name="links")
-    async def cmd_links(self, ctx, group=None):
+    async def cmd_links(self, ctx, category=None):
         if channel_links := self.links.get(str(ctx.channel.id)):
             embed = discord.Embed(title=f"Folgende Links sind in diesem Channel hinterlegt:\n")
-            if group:
-                group = group.lower()
-                if group_links := channel_links.get(group):
+            if category:
+                category = category.lower()
+                if group_links := channel_links.get(category):
                     value = f""
                     for title, link in group_links.items():
                         value += f"- [{title}]({link})\n"
-                    embed.add_field(name=group.capitalize(), value=value, inline=False)
+                    embed.add_field(name=category.capitalize(), value=value, inline=False)
                     await ctx.send(embed=embed)
                 else:
                     await ctx.send(
-                        f" Für die Gruppe `{group}` sind in diesem Channel keine Links hinterlegt. Versuch es noch mal mit einer anderen Gruppe, oder lass dir mit `!links` alle Links in diesem Channel ausgeben")
+                        f" Für die Kategorie `{category}` sind in diesem Channel keine Links hinterlegt. Versuch es noch mal mit einer anderen Gruppe, oder lass dir mit `!links` alle Links in diesem Channel ausgeben")
             else:
-                for group, links in channel_links.items():
+                for category, links in channel_links.items():
                     value = f""
                     for title, link in links.items():
                         value += f"- [{title}]({link})\n"
-                    embed.add_field(name=group.capitalize(), value=value, inline=False)
+                    embed.add_field(name=category.capitalize(), value=value, inline=False)
                 await ctx.send(embed=embed)
         else:
             await ctx.send("Für diesen Channel sind noch keine Links hinterlegt.")
 
     @help(
         category="links",
-        syntax="!add-link <group> <link> <title...>",
+        syntax="!add-link <category> <link> <title...>",
         brief="Fügt einen Link zum Channel hinzu.",
         parameters={
-            "group": "Name der Gruppe, die der Link zugeordnet werden soll. ",
+            "category": "Name der Kategorie, der der Link zugeordnet werden soll. ",
             "link": "die URL, die aufgerufen werden soll (z. B. https://www.fernuni-hagen.de). ",
             "title...": "Titel, der für diesen Link angezeigt werden soll (darf Leerzeichen enthalten). ",
         },
         description="Die mit !add-link zu einem Kanal hinzugefügten Links können über das Kommando !links in diesem Kanal wieder abgerufen werden."
     )
     @commands.command(name="add-link")
-    async def cmd_add_link(self, ctx, group, link, *title):
+    async def cmd_add_link(self, ctx, category, link, *title):
+        category = category.lower()
         if not (channel_links := self.links.get(str(ctx.channel.id))):
             self.links[str(ctx.channel.id)] = {}
             channel_links = self.links.get(str(ctx.channel.id))
 
-        if not (group_links := channel_links.get(group)):
-            channel_links[group] = {}
-            group_links = channel_links.get(group)
+        if not (group_links := channel_links.get(category)):
+            channel_links[category] = {}
+            group_links = channel_links.get(category)
 
         self.add_link(group_links, link, " ".join(title))
         self.save_links()
