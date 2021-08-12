85c17b60d01571d06e3330880c864559534c6fa3
samari-k <safi.kuehn@gmx.de>
2021-08-12 20:50:18 +0200

    split remove into remove-link and remove-topic

--- cogs/links.py
+++ cogs/links.py
@@ -50,7 +50,7 @@ class Links(commands.Cog):
                     for title, link in links.items():
                         value += f"- [{title}]({link})\n"
                     embed.add_field(name=topic.capitalize(), value=value, inline=False)
-            await ctx.send(embed=embed)
+                await ctx.send(embed=embed)
         else:
             await ctx.send("Für diesen Channel sind noch keine Links hinterlegt.")
 
@@ -88,31 +88,28 @@ class Links(commands.Cog):
 
     @help(
         category="links",
-        syntax="!links remove <topic> <title...?>",
-        brief="Löscht ein Thema oder einen Link aus dem Channel.",
+        syntax="!links remove-link <topic> <title...>",
+        brief="Löscht einen Link aus dem Channel.",
         parameters={
             "topic": "Name des Themas, aus dem der Link entfernt werden soll. ",
-            "title...": "*(optional)* Titel des Links, der entfernt werden soll. "
+            "title...": "Titel des Links, der entfernt werden soll. "
         },
-        description="Mit !links remove kann ein ganzes Thema oder ein einzelner fehlerhafter oder veralteter Link "
-                    "aus der Linkliste des Channels entfernt werden."
+        description="Mit !links remove-link kann ein fehlerhafter oder veralteter Link aus der Linkliste des Channels "
+                    "entfernt werden."
     )
-    @cmd_links.command(name="remove")
+    @cmd_links.command(name="remove-link", aliases=['rl'])
     async def cmd_remove_link(self, ctx, topic, *title):
         topic = topic.lower()
+        title = " ".join(title)
 
         if channel_links := self.links.get(str(ctx.channel.id)):
             if topic_links := channel_links.get(topic):
-                if title:
-                    title = " ".join(title)
-                    if topic_links.get(title):
-                        topic_links.pop(title)
-                        if not topic_links:
-                            channel_links.pop(topic)
-                    else:
-                        await ctx.channel.send('Ich konnte den Link leider nicht finden.')
+                if topic_links.get(title):
+                    topic_links.pop(title)
+                    if not topic_links:
+                        channel_links.pop(topic)
                 else:
-                    channel_links.pop(topic)
+                    await ctx.channel.send('Ich konnte den Link leider nicht finden.')
             else:
                 await ctx.channel.send('Ich konnte das Thema leider nicht finden.')
         else:
@@ -122,21 +119,61 @@ class Links(commands.Cog):
 
     @help(
         category="links",
-        syntax="!links edit-link <topic> <title> <new_topic> <new_link> <new_title...>",
+        syntax="!links remove-topic <topic>",
+        brief="Löscht eine komplette Themenkategorie aus dem Channel.",
+        parameters={
+            "topic": "Name des Themas, das entfernt werden soll. ",
+        },
+        description="Mit !links remove-topic kann ein Thema aus der Linkliste des Channels entfernt werden."
+    )
+    @cmd_links.command(name="remove-topic", aliases=['rt'])
+    async def cmd_remove_topic(self, ctx, topic):
+        topic = topic.lower()
+
+        if channel_links := self.links.get(str(ctx.channel.id)):
+            if channel_links.get(topic):
+                channel_links.pop(topic)
+            else:
+                await ctx.channel.send('Ich konnte das Thema leider nicht finden.')
+        else:
+            await ctx.channel.send('Für diesen Channel sind keine Links hinterlegt.')
+
+        self.save_links()
+
+
+    @help(
+        category="links",
+        syntax="!links edit-link <topic> <title> <new_title> <new_topic?> <new_link?>",
         brief="Bearbeitet einen Link.",
         parameters={
             "topic": "Name des Themas, aus dem der zu bearbeitende Link stammt. ",
             "title": "Titel des Links, der bearbeitet werden soll. ",
-            "new_topic": "Neues Thema für den geänderten Link. ",
-            "new_link": "Der neue Link. ",
-            "new_title...": "Neuer Titel für den geänderten Link. "
+            "new_title": "Neuer Titel für den geänderten Link. ",
+            "new_topic": "*(optional)* Neues Thema für den geänderten Link. ",
+            "new_link": "*(optional)* Der neue Link. "
         },
         description="Mit !links edit-link kann ein fehlerhafter oder veralteter Link bearbeitet werden."
     )
-    @cmd_links.command(name="edit-link")
-    async def cmd_edit_link(self, ctx, topic, title, new_topic, new_link, *new_title):
+    @cmd_links.command(name="edit-link", aliases=["el"])
+    async def cmd_edit_link(self, ctx, topic, title, new_title, new_topic=None, new_link=None):
+
+        if not new_topic:
+            new_topic = topic
+
+        if not new_link:
+            if channel_links := self.links.get(str(ctx.channel.id)):
+                if topic_links := channel_links.get(topic):
+                    if topic_links.get(title):
+                        new_link = topic_links.get(title)
+                    else:
+                        await ctx.channel.send('Ich konnte den Link leider nicht finden.')
+                else:
+                    await ctx.channel.send('Ich konnte das Thema leider nicht finden.')
+            else:
+                await ctx.channel.send('Für diesen Channel sind keine Links hinterlegt.')
+
         await self.cmd_remove_link(ctx, topic, title)
-        await self.cmd_add_link(ctx, new_topic, new_link, *new_title)
+        await self.cmd_add_link(ctx, new_topic, new_link, new_title)
 
     @help(
         category="links",
@@ -148,14 +185,14 @@ class Links(commands.Cog):
         },
         description="Mit !links edit-topic kann der Name eines Themas geändert werden."
     )
-    @cmd_links.command(name="edit-topic")
+    @cmd_links.command(name="edit-topic", aliases=["et"])
     async def cmd_edit_topic(self, ctx, topic, new_topic):
         topic = topic.lower()
         new_topic = new_topic.lower()
         if channel_links := self.links.get(str(ctx.channel.id)):
             if topic_links := channel_links.get(topic):
                 channel_links[new_topic] = topic_links
-                await self.cmd_remove_link(ctx, topic)
+                await self.cmd_remove_topic(ctx, topic)
             else:
                 await ctx.channel.send('Ich konnte das Thema leider nicht finden.')
         else:
