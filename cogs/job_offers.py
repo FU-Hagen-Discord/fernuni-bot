388c622a2467cbcc64edbe44cbe61299025e6519
samari-k <safi.kuehn@gmx.de>
2022-02-25 08:34:20 +0100

    cahnge all requested changes

--- cogs/job_offers.py
+++ cogs/job_offers.py
@@ -21,6 +21,8 @@ from cogs.help import help
   mit fak = [mi|rewi|wiwi|ksw|psy]
 """
 
+JOBS_URL = os.getenv("DISCORD_JOBOFFERS_URL")
+STD_FAK = os.getenv("DISCORD_JOBOFFERS_STD_FAK")
 
 class Joboffers(commands.Cog):
     def __init__(self, bot):
@@ -28,8 +30,6 @@ class Joboffers(commands.Cog):
         self.joboffers = {}
         self.joboffers_channel_id = int(os.getenv("DISCORD_JOBOFFERS_CHANNEL"))
         self.joboffers_file = os.getenv("DISCORD_JOBOFFERS_FILE")
-        self.jobs_url = os.getenv("DISCORD_JOBOFFERS_URL")
-        self.stad_fak = os.getenv("DISCORD_JOBOFFERS_STD_FAK")
         self.load_joboffers()
         self.update_loop.start()
 
@@ -53,9 +53,9 @@ class Joboffers(commands.Cog):
             self.joboffers = {}
 
     @help(
-        syntax="!jobs <fak?>",
+        syntax="/jobs <fak?>",
         parameters={
-            "fak": "*(optional)* Fakultät für die die studentische Hilfskraft Jobs ausgegeben werden sollen "
+            "fak": "Fakultät für die die studentische Hilfskraft Jobs ausgegeben werden sollen "
                    "(mi, rewi, wiwi, ksw, psy)"
         },
         brief="Ruft Jobangebote für Studiernde der Fernuni Hagen auf."
@@ -70,23 +70,22 @@ class Joboffers(commands.Cog):
                                                              disnake.OptionChoice('ksw','ksw'),
                                                              disnake.OptionChoice('psy','psy'),
                                                              disnake.OptionChoice('other','other')])])
-    async def cmd_jobs(self, interaction: ApplicationCommandInteraction, faculty: str):
+    async def cmd_jobs(self, interaction: ApplicationCommandInteraction, faculty: str = STD_FAK):
         await self.fetch_joboffers()
 
         embed = disnake.Embed(title="Stellenangebote der Uni",
                               description=f"Ich habe folgende Stellenangebote der Fakultät {faculty} gefunden:")
         if offers := self.joboffers.get(faculty):
-            for offer in offers:
-                offer = offers[offer]
-                descr = f"{offer['info']}\nDeadline: {offer['deadline']}\n{offer['link']}"
-                embed.add_field(name=offer['title'], value=descr, inline=False)
+            for offer_id, offer_data in self.joboffers.get(faculty).items():
+                descr = f"{offer_data['info']}\nDeadline: {offer_data['deadline']}\n{offer_data['link']}"
+                embed.add_field(name=offer_data['title'], value=descr, inline=False)
 
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
     async def post_new_jobs(self, jobs):
         embed = disnake.Embed(title="Neue Stellenangebote der Uni",
-                              description=f"Ich habe folgende neue Stellenangebote der Fakultät {self.stad_fak} gefunden:")
+                              description=f"Ich habe folgende neue Stellenangebote der Fakultät {STD_FAK} gefunden:")
         for job in jobs:
             descr = f"{job['info']}\nDeadline: {job['deadline']}\n{job['link']}"
             embed.add_field(name=job['title'], value=descr, inline=False)
@@ -97,7 +96,7 @@ class Joboffers(commands.Cog):
 
     async def fetch_joboffers(self):
         sess = aiohttp.ClientSession()
-        req = await sess.get(self.jobs_url)
+        req = await sess.get(JOBS_URL)
         text = await req.read()
         await sess.close()
 
@@ -136,18 +135,16 @@ class Joboffers(commands.Cog):
 
     async def check_for_new_jobs(self, old_joboffers):
         new_jobs = []
-        if fak := self.joboffers.get(self.stad_fak):
-            if fak_old := old_joboffers.get(self.stad_fak):
-                for offer_id in fak:
-                    offer = fak[offer_id]
+        if fak := self.joboffers.get(STD_FAK):
+            if fak_old := old_joboffers.get(STD_FAK):
+                for offer_id, offer_data in self.joboffers.get(STD_FAK).items():
                     if old_offer := fak_old.get(offer_id):
-                        if offer != old_offer:
-                            new_jobs.append(offer)
+                        if offer_data != old_offer:
+                            new_jobs.append(offer_data)
                     else:
-                        new_jobs.append(offer)
+                        new_jobs.append(offer_data)
             else:
-                for offer_id in fak:
-                    offer = fak[offer_id]
-                    new_jobs.append(offer)
+                for offer_id, offer_data in self.joboffers.get(STD_FAK).items():
+                    new_jobs.append(offer_data)
         if new_jobs:
             await self.post_new_jobs(new_jobs)
